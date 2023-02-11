998f8ab5d80b50426e219d6231c5c1b188725f6b
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-02-11 14:34:28 +0000

    Added clear button

---
+++ script/main.js
@@ -0,0 +1,34 @@
+// Pseudo Code
+
+// Create a weather dashboard with form inputs. - done
+
+// When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history.
+
+// When a user views the current weather conditions for that city they are presented with:
+// The city name
+// The date
+// An icon representation of weather conditions
+// The temperature
+// The humidity
+// The wind speed
+
+// When a user views future weather conditions for that city they are presented with a 5-day forecast that displays:
+// The date
+// An icon representation of weather conditions
+// The temperature
+// The humidity
+// When a user clicks on a city in the search history they are again presented with current and future conditions for that city.
+
+// Api call for metric units
+
+// https://api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&appid={API key}&units=metric
+
+
+const apiKey = '2253c82eaacce5516e8db0f622c8e22a';
+const cityInput = document.querySelector('#search-city');
+const submitBtn = document.querySelector('#search-button');
+const clearBtn = document.querySelector('#clear-button');
+
+
+
+
