c1ab73a98543367fd7ca611b314113fb2be1cd57
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-02-11 21:13:11 +0000

    Added all variables

--- script/main.js
+++ script/main.js
@@ -24,11 +24,26 @@
 // https://api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&appid={API key}&units=metric
 
 
+// Variables 
+
 const apiKeyWeather = '2253c82eaacce5516e8db0f622c8e22a';
 const cityInput = document.querySelector('#search-city');
 const submitBtn = document.querySelector('#search-button');
 const clearBtn = document.querySelector('#clear-button');
 
+const forecast = document.querySelector('#forecast');
+const input = document.querySelector('.form-input');
+const currentCity = document.querySelector('#currentCity');
+const currentTemp = document.querySelector('#currentTemp');
+const currentWind = document.querySelector('#currentWind');
+const currentHumid = document.querySelector('#currentHumid');
+
+const buttonList = document.querySelector('.btnList');
+
+var location;
+var lat;
+var lon;
+var counter;
 
 // EVENT LISTENER FOR SUBMIT BUTTON TO GET CITY
 
