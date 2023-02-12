a9084df94fc019ce1e4b475dcae7605ada22f9ad
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-02-12 19:48:54 +0000

    added api ajax call for current weather

--- script/main.js
+++ script/main.js
@@ -26,7 +26,7 @@
 
 // Variables 
 
-const apiKeyWeather = '2253c82eaacce5516e8db0f622c8e22a';
+const APIKey = '2253c82eaacce5516e8db0f622c8e22a';
 const cityInput = document.querySelector('#search-city');
 const submitBtn = document.querySelector('#search-button');
 const clearBtn = document.querySelector('#clear-button');
@@ -45,12 +45,41 @@ var lat;
 var lon;
 var counter;
 
+
 // EVENT LISTENER FOR SUBMIT BUTTON TO GET CITY
 
+var locationEntry;
+
+$(".submit-btn").click(function(event) {
+    event.preventDefault();
+    locationEntry = ($("#search-city").val());
+    console.log(locationEntry);
 
 
- 
+  // CALL TO GEO API FOR LAT AND LON
+  var apiKeyWeather = "def3fa492eb50ea6560f2cae7d9d29e7";
+  var queryUrlGeocode = "https://api.openweathermap.org/geo/1.0/direct?q="+ locationEntry + "&limit=1&appid="+ apiKeyWeather;
+  $.ajax({
+    url: queryUrlGeocode,
+    method: "GET"
+  }).then(function(response) {
+      var lat = response[0].lat;
+      var lon = response[0].lon;
+      var queryUrlWeather = "https://api.openweathermap.org/data/2.5/weather?lat="+ lat + "&lon=" + lon +"&appid=" + apiKeyWeather + "&units=metric";
+      
+  // CALL TO WEATHER API FOR CURRENT WEATHER
+  $.ajax({
+      url: queryUrlWeather,
+      method: "GET"
+  }).then(function(response) {
+     console.log(response) // <<< THIS IS API RESPONSE AFTER YOU ENTER CITY NAME AND PRESS SEARCH
+    }); 
 
+  });
+});
+
+
+    
 
 
 
